bb59d617c203974079b64f72a965633dd1345a26
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-11-18 15:21:59 -0300

    feat(req-01): resolução - numero de pessoas estudantes acessando o conteudo no mesmo horario

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,24 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period: list[tuple], target_time: int):
+    # qual horário tem a maior quantidade de pessoas
+    # estudantes acessando o conteúdo da plataforma?
+
+    # permanence_period (entrada, saida)
+    # [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
+    if not isinstance(target_time, int):
+        return None
+
+    counter: int = 0
+
+    for (entry_period, exit_period) in permanence_period:
+        if (
+            not entry_period
+            or not exit_period
+            or isinstance(entry_period, str)
+            or isinstance(exit_period, str)
+        ):
+            return None
+        if entry_period <= target_time <= exit_period:
+            counter = counter + 1
+
+    return counter
