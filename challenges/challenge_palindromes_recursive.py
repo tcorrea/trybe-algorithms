06b987522b9d20111a6876c86d34a9f81727a10b
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-11-18 18:23:54 -0300

    feat(req-3): palindromes usando recursividade

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,33 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(
+    word: str, low_index: int, high_index: int
+) -> bool:
+    """
+    Parameters
+    ----------
+    word: str
+    low_index: int
+    high_index: int
+
+    Returns
+    -------
+    bool
+
+    """
+    # verifica se existe word: retorna False
+    if not word:
+        return False
+
+    # retorna True se for passado somente uma letra no parametro word
+    if low_index == high_index:
+        return True
+
+    # se a letra no começo da palavra não corresponde com a letra do final
+    if word[low_index] != word[high_index]:
+        return False
+
+    # parada recursividade
+    # enquanto low for menor que high, a propria função será chamada
+    if low_index < high_index:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return True
