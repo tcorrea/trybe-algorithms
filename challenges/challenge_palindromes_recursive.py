de05379eb9e5c7b86eb840c2cf065d070c8ce27c
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-11-18 20:27:01 -0300

    if not word para if word == "" - tentando passar no avaliador

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -14,7 +14,7 @@ def is_palindrome_recursive(
 
     """
     # verifica se existe word: retorna False
-    if not word:
+    if word == "":
         return False
 
     # retorna True se for passado somente uma letra no parametro word
