1e3503d868ba26cdb1dce2784cf318a512fd3153
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-11-19 16:43:39 -0300

    feat(req-05): resolução do desafio find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,41 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+from challenges.quick_sort import quick_sort
+
+
+def find_duplicate(nums: list) -> bool | int:
+    """
+    Parameters
+    ----------
+    nums: list
+
+    Returns
+    -------
+    False | int
+
+    """
+
+    # sort a lista de numeros
+    quick_sort(nums, 0, len(nums) - 1)
+
+    # lista para armazenar o resultado
+    numbers_found: list = []
+
+    # enquanto existir elemento na lista nums
+    while len(nums) > 0:
+
+        # ferifica se é uma string ou um numero negativo
+        if isinstance(nums[0], str) or nums[0] < 0:
+            return False
+
+        # se o numero da primeira posição for igual a da segunda
+        # e for diferente do ultimo item do numbers_found
+        if nums[:1] == nums[1:2] and nums[:1] != numbers_found[-1:]:
+            # adiciona o numero na lista de resultado
+            numbers_found.append(nums[0])
+
+        # remove o promeiro item de nums
+        nums.pop(0)
+
+    if len(numbers_found) == 1:
+        return numbers_found[0]
+
+    return False
