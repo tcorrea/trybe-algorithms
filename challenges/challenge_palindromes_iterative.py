f0214ecc2166f294194a3e2e89fe9c5e5925e71a
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-11-19 18:31:57 -0300

    feat(req-06): solução do desafio palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,25 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word: str):
+    """
+    Parameters
+    ----------
+    word: str
+
+    Returns
+    -------
+    bool
+
+    """
+    if not word:
+        return False
+
+    start: int = 0
+    end: int = len(word) - 1
+
+    while start < end:
+        if word[start] != word[end]:
+            return False
+
+        start = start + 1
+        end = end - 1
+
+    return True
